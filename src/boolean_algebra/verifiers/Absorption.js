f535b9bb9b84525b7b0bea48bcaa71cc19f8d383
devanshuh <haldad@rpi.edu>
2022-04-28 18:47:05 -0400

    absorption and annnihilation

--- src/boolean_algebra/verifiers/Absorption.js
+++ src/boolean_algebra/verifiers/Absorption.js
@@ -1,10 +1,46 @@
-import {getParsedStatement} from "../Parser.js";
+import {getParsedStatement, getString} from "../Parser.js";
 
 export function AbsorptionVerifier(statement1, statement2) {
-  return false;
+  return AbsoprtionHelper(statement1,statement2);
 }
 
-let statement1 = getParsedStatement("A&(A|B)")
+function AbsoprtionHelper(statement1,statement2){
+  let connective1 = statement1.type;
+  let LHS = statement1.parts[0];
+  let RHS = statement1.parts[1];
+  if (RHS.parts.length === 2){
+    if (getString(RHS.parts[0]) === getString(LHS)){
+      if (connective1 === "AND" && RHS.type === "OR") 
+        return true; 
+      else if (connective1 === "OR" && RHS.type === "AND") 
+        return true;
+    }
+  }
+  return false
+}
+
+
+let statement1 = getParsedStatement("A&(A|B)")//true
 let statement2 = getParsedStatement("A")
 
+let statement3 = getParsedStatement("A|(A&B)")//true
+let statement4 = getParsedStatement("A")
+
+let statement5 = getParsedStatement("A|(A|B)")//false
+let statement6 = getParsedStatement("A")
+
+let statement7 = getParsedStatement("A&(A&B)")//false
+let statement8 = getParsedStatement("A")
+
+let statement9 = getParsedStatement("(A|B)&((A&B)|C)")//false
+let statement10 = getParsedStatement("(A&B)")
+
+let statement11 = getParsedStatement("(A&B)&((A&B)|C)")//true
+let statement12 = getParsedStatement("(A&B)")
+
 console.log(AbsorptionVerifier(statement1, statement2))
+console.log(AbsorptionVerifier(statement3, statement4))
+console.log(AbsorptionVerifier(statement5, statement6))
+console.log(AbsorptionVerifier(statement7, statement8))
+console.log(AbsorptionVerifier(statement9,statement10))
+console.log(AbsorptionVerifier(statement11,statement12))
--- src/boolean_algebra/verifiers/Annihilation.js
+++ src/boolean_algebra/verifiers/Annihilation.js
@@ -1,3 +1,43 @@
+import {getParsedStatement, getString} from "../Parser.js";
+
 export function AnnihilationVerifier(statement1, statement2) {
+  return (AnnihilationHelper(statement1, statement2) || AnnihilationHelper(statement2,statement1));
+}
+
+function AnnihilationHelper(statement1,statement2){
+  if (statement1.parts.length !== 2) return false;
+  let connective1 = statement1.type;
+  if (getString(statement1.parts[1]) === getString(statement2)){
+    if (statement1.parts[1].parts.length === 1) {
+      if (statement1.parts[1].parts[0] === "⊥" && connective1 === "AND") {
+        return true;
+      }
+      if (statement1.parts[1].parts[0] === "⊤" && connective1 === "OR") {
+        return true;
+      }
+    }
+  }
   return false;
 }
+
+
+let statement1 = getParsedStatement("A|⊤")//true
+let statement2 = getParsedStatement("⊤")
+
+let statement3 = getParsedStatement("A&⊥")//true
+let statement4 = getParsedStatement("⊥")
+
+let statement5 = getParsedStatement("(A&B)&⊥")//true
+let statement6 = getParsedStatement("⊥")
+
+let statement7 = getParsedStatement("(A|B)|⊤")//false
+let statement8 = getParsedStatement("T")
+
+let statement9 = getParsedStatement("A&B&C&D&⊥")//false
+let statement10 = getParsedStatement("⊥");
+
+console.log(AnnihilationVerifier(statement1,statement2))
+console.log(AnnihilationVerifier(statement3,statement4))
+console.log(AnnihilationVerifier(statement5,statement6))
+console.log(AnnihilationVerifier(statement7,statement8))
+console.log(AnnihilationVerifier(statement9,statement10))
