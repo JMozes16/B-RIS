00211548d49ba8813e985f63e98129412f6b57ae
Josh Mozes <85561037+JMozes16@users.noreply.github.com>
2022-04-27 23:05:18 -0400

    Fixes

--- src/boolean_algebra/verifiers/Distribution.js
+++ src/boolean_algebra/verifiers/Distribution.js
@@ -1,38 +1,101 @@
-import * as Parser from "../Parser";
+import {getParsedStatement, getString} from "../Parser.js";
 
 export function DistributionVerifier(parsedStatement1, parsedStatement2) {
-  return(Distribution(parsedStatement1, parsedStatement2) || Distribution(parsedStatement2, parsedStatement1));
+  if (getString(parsedStatement2).length > getString(parsedStatement1).length) {
+    return(Distribution(parsedStatement1, parsedStatement2));
+  } else if (getString(parsedStatement1).length > getString(parsedStatement2).length) {
+    return(Distribution(parsedStatement2, parsedStatement1));
+  } else {
+    return(Distribution(parsedStatement2, parsedStatement1) || Distribution(parsedStatement1, parsedStatement2));
+  }
 }
 
 function Distribution(parsedStatement1, parsedStatement2) {
   let connective1 = parsedStatement1.type;
-  let newStr = DistributionHelper(parsedStatement1[0], parsedStatement1[1], connective1);
-  for (let i = 1; i<parsedStatement1.parts.length-1; i++) {
-    let newStr = DistributionHelper(Parser.parseStatement(newStr), parsedStatement1[i+1], connective1); //problem is that newstr is a string not type
+  let allAtomic = true;
+  for (let i = 0; i < parsedStatement1.parts.length; i++) {
+    if (parsedStatement1.parts[i].type !== "ATOMIC") {
+      allAtomic = false;
+      break;
+    }
   }
-  return (newStr === Parser.getString(parsedStatement2));
+  if (allAtomic) {
+    console.log("All Atomic");
+    return false
+  }
+  let newStr = DistributionHelper(parsedStatement1.parts[0], parsedStatement1.parts[1], connective1);
+  if (newStr === getString(parsedStatement2)) {
+    console.log(newStr);
+    console.log(getString(parsedStatement2));
+    return true
+  }
+  for (let i = 1; i<parsedStatement1.parts.length-1; i++) {
+    newStr = DistributionHelper(getParsedStatement(newStr), parsedStatement1.parts[i+1], connective1); //problem is that newstr is a string not type
+    console.log(newStr);
+    console.log(getString(parsedStatement2));
+    if (newStr === getString(parsedStatement2)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function DistributionHelper(LHS, RHS, connective1) {
+  let distString = "(";
   if (LHS.type === "ATOMIC" && RHS.type === "ATOMIC") {
-    let distString = "";
-    distString += "(" + Parser.getString(LHS.parts[0]) + connective1 + Parser.getString(RHS.parts[0]) + ")";
+    distString += "(" + getString(LHS.parts[0]) + connective1 + getString(RHS.parts[0]) + ")";
   } else if ((LHS.type !== "NOT") && (RHS.type !== "NOT")) {
     for (let i = 0; i < LHS.parts.length; i++) {
       for (let j = 0; j < RHS.parts.length; j++) {
-        if (i != LHS.parts.length-1 && j != RHS.parts.length-1) {
-          if (LHS.type === "ATOMIC") {
-            distString += "(" + Parser.getString(LHS.parts[i]) + connective1 + Parser.getString(RHS.parts[j]) + ")" + RHS.type;
-          } else {
-            distString += "(" + Parser.getString(LHS.parts[i]) + connective1 + Parser.getString(RHS.parts[j]) + ")" + LHS.type;
+        if ((i != LHS.parts.length-1) || (j != RHS.parts.length-1)) {
+          if (LHS.type !== "ATOMIC" && RHS.type !== "ATOMIC") {
+            distString += "(" + getString(LHS.parts[i]) + connective1 + getString(RHS.parts[j]) + ")" + LHS.type;
+          } else if (LHS.type !== "ATOMIC" && RHS.type === "ATOMIC") {
+            distString += "(" + getString(LHS.parts[i]) + connective1 + RHS.parts[j] + ")" + LHS.type;
+          } else if (LHS.type === "ATOMIC" && RHS.type !== "ATOMIC") {
+            distString += "(" + LHS.parts[i] + connective1 + getString(RHS.parts[j]) + ")" + RHS.type;
+          }
+        } else {
+          if (LHS.type !== "ATOMIC" && RHS.type !== "ATOMIC") {
+            distString += "(" + getString(LHS.parts[i]) + connective1 + getString(RHS.parts[j]) + ")";
+          } else if (LHS.type !== "ATOMIC" && RHS.type === "ATOMIC") {
+            distString += "(" + getString(LHS.parts[i]) + connective1 + RHS.parts[j] + ")";
+          } else if (LHS.type === "ATOMIC" && RHS.type !== "ATOMIC") {
+            distString += "(" + LHS.parts[i] + connective1 + getString(RHS.parts[j]) + ")";
           }
         }
-        distString += "(" + Parser.getString(LHS.parts[i]) + connective1 + Parser.getString(RHS.parts[j]) + ")";
       }
     }
   }
-  distString.replace("AND", "&");
-  distString.replace("OR", "|");
-  distString.replace("NOT", "~");
+  distString += ")";
+  distString = distString.replaceAll("AND", "&");
+  distString = distString.replaceAll("OR", "|");
+  distString = distString.replaceAll("NOT", "~");
   return distString;
 }
+
+let statement1 = getParsedStatement("A|(B&C)")
+let statement2 = getParsedStatement("(A|B)&(A|C)")
+
+let statement3 = getParsedStatement("(A&B)|(C&D)")
+let statement4 = getParsedStatement("(A|C)&(A|D)&(B|C)&(B|D)")
+
+let statement5 = getParsedStatement("(A|B)&(C|D)&(E|F)")
+let statement6 = getParsedStatement("(((A&C)&E)|((A&C)&F)|((A&D)&E)|((A&D)&F)|((B&C)&E)|((B&C)&F)|((B&D)&E)|((B&D)&F))")
+
+let statement7 = getParsedStatement("(A|B)&(C|D)&(E|F)")
+let statement8 = getParsedStatement("(((A&C)&E)|((A&C)&F)|((A&D)&E)|((A&D)&F)|((B&C)&E)|((B&C)&F)|((B&D)&E)|((B&D)&E))")
+
+let statement9 = getParsedStatement("(A|B)&(C|(D&E))")
+let statement10 = getParsedStatement("(A&C)|(A&(D&E))|(B&C)|(B&(D&E))")
+
+let statement11 = getParsedStatement("A|A")
+let statement12 = getParsedStatement("A|A")
+
+console.log(DistributionVerifier(statement1, statement2))
+console.log(DistributionVerifier(statement2, statement1))
+console.log(DistributionVerifier(statement3, statement4))
+console.log(DistributionVerifier(statement5, statement6))
+console.log(DistributionVerifier(statement7, statement8))
+console.log(DistributionVerifier(statement9, statement10))
+console.log(DistributionVerifier(statement11, statement12))
