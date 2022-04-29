08bfcfd1d0fc9ce338428391059a319ea12ad242
PrinxeShamar <44654743+PrinxeShamar@users.noreply.github.com>
2022-04-29 00:13:10 -0400

    C

--- src/boolean_algebra/verifiers/DeMorgan.js
+++ src/boolean_algebra/verifiers/DeMorgan.js
@@ -1,3 +1,46 @@
+import {getString} from "../Parser";
+
+function DeMorgan(statement) {
+  let newStatement = {
+    type: "",
+    parts: [],
+  }
+  if (statement.parts[0].type === "AND") {
+    newStatement.type = "OR"
+  } else if(statement.parts[0].type === "OR") {
+    newStatement.type = "AND"
+  }
+  for (let i = 0; i < statement.parts[0].parts.length; i++) {
+    newStatement.parts.push({
+      type: "NOT",
+      parts: [statement.parts[0].parts[i]],
+    })
+  }
+  return newStatement;
+}
+
 export function DeMorganVerifier(statement1, statement2) {
-  return false;
+  if (getString(statement1) === getString(statement2)) {
+    return true
+  }
+  if (statement1.type === "AND" || statement1.type === "OR") {
+    if ((statement1.type !== statement2.type) ||
+        (statement1.parts.length !== statement2.parts.length)) {
+      return false
+    }
+    for (let i = 0; i < statement1.parts.length; i++) {
+      if (!DeMorganVerifier(statement1.parts[i], statement2.parts[i])) {
+        return false
+      }
+    }
+    return true
+  } else if(statement1.type === "NOT") {
+    if (statement2.type === "NOT") {
+      return DeMorganVerifier(statement1.parts[0], statement2.parts[0])
+    } else {
+      return getString(DeMorgan(statement1)) === getString(statement2)
+    }
+  } else {
+    return false
+  }
 }
