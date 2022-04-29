ab2c3c3da6b64fc14b6f272236cc1233356af526
Josh Mozes <85561037+JMozes16@users.noreply.github.com>
2022-04-29 03:53:59 -0400

    FindChange Idempotence

--- src/boolean_algebra/verifiers/Idempotence.js
+++ src/boolean_algebra/verifiers/Idempotence.js
@@ -1,13 +1,62 @@
 import {getParsedStatement, getString} from "../Parser.js";
 
 export function IdempotenceVerifier(statement1, statement2) {
-  return IdempotenceHelper(statement1,statement2);
+  let state1 = statement1;
+  let state2 = statement2;
+  if (getString(statement1).length > getString(statement2).length) {
+    state1 = statement1;
+    state2 = statement2;
+  } else if (getString(statement2).length > getString(statement1).length) {
+    state1 = statement2;
+    state2 = statement1;
+  } else {
+    return false;
+  }
+  return FindChanges(state1, state2, getString(state1), getString(state2));
+}
+
+function FindChanges(state1, state2, str1, str2) {
+  if (IdempotenceHelper(state1, state2)) {
+    return true;
+  }
+  let spot = -1;
+  if (!state2.type) {
+    return false;
+  }
+  for (let i=0; i<state2.parts.length; i++) {
+    if (state2.type === state1.type) {
+      if (state2.type === "ATOMIC") {
+        if (state2.parts[i].type !== state1.parts[i].type || state2.parts[i] !== state1.parts[i]) {
+          spot = i;
+        }
+      } else {
+        if (state2.parts[i].type !== state1.parts[i].type || getString(state2.parts[i]) !== getString(state1.parts[i])) {
+          spot = i;
+        }
+      }
+    } else {
+      return IdempotenceHelper(state1, state2);
+    }
+  }
+  if (spot === -1) {
+    return false;
+  } else {
+    if (FindChanges(state1.parts[spot], state2.parts[spot], str1, str2)) {
+      if (str1.replace(getString(state1.parts[spot]), getString(state2.parts[spot])) === str2) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 function IdempotenceHelper(statement1,statement2){
   let count = false;
   let answers = []; 
   let str = "(";
+  if (statement1.type !== "AND" && statement1.type !== "OR") {
+    return false;
+  }
   for (let i = 0; i < statement1.parts.length-1; i++){
     if (getString(statement1.parts[i]) == getString(statement1.parts[i+1])){
       if (statement1.parts[i].type !== "ATOMIC") {
@@ -38,8 +87,6 @@ function IdempotenceHelper(statement1,statement2){
       }
     }
     if (statement2.type !== "ATOMIC") answers[i] += ")";
-    console.log("Ans",answers[i]);
-    console.log("state2",getString(statement2))
     if (getString(statement2) == answers[i]) return true;
   }
 
@@ -60,8 +107,22 @@ let statement7 = getParsedStatement("A&A&B&B");
 
 let statement8 = getParsedStatement("(A&B)&(A&B)&(A&B)");
 let statement9 = getParsedStatement("(A&B)&(A&B)");
+
+let statement10 = getParsedStatement("((A&B)&(A&B)&(A&B))&(E|F)");
+let statement11 = getParsedStatement("((A&B)&(A&B))&(E|F)");
+
+let statement12 = getParsedStatement("(C|D)&((A&B)&(A&B)&(A&B))&(E|F)");
+let statement13 = getParsedStatement("(C|D)&((A&B)&(A&B))&(E|G)");
+
+let statement14 = getParsedStatement("(C|D)&((A&B)&(A&B)&(A&B))&(E|F)");
+let statement15 = getParsedStatement("(C|D)&((A&B)&(A&B))&(E|F)");
+
 console.log(IdempotenceVerifier(statement3,statement1)); //true
 console.log(IdempotenceVerifier(statement3,statement2)); //true 
-console.log(IdempotenceVerifier(statement4,statement5));
-console.log(IdempotenceVerifier(statement6,statement7))
-console.log(IdempotenceVerifier(statement8,statement9))
+console.log(IdempotenceVerifier(statement4,statement5)); //true
+console.log(IdempotenceVerifier(statement6,statement7)); //true
+console.log(IdempotenceVerifier(statement8,statement9)); //true
+console.log(IdempotenceVerifier(statement10,statement11)); //true
+console.log(IdempotenceVerifier(statement12,statement13)); //false
+console.log(IdempotenceVerifier(statement14,statement15)); //true
+
