48b074dc881f09641cf4088649e1a1af31b9ea05
Josh Mozes <85561037+JMozes16@users.noreply.github.com>
2022-04-29 02:35:02 -0400

    FindChanges Reduction

--- src/boolean_algebra/verifiers/Reduction.js
+++ src/boolean_algebra/verifiers/Reduction.js
@@ -1,13 +1,44 @@
 import {getParsedStatement, getString} from "../Parser.js";
 
 export function ReductionVerifier(statement1, statement2) {
+  let state1 = statement1;
+  let state2 = statement2;
   if (getString(statement1).length > getString(statement2).length) {
-    return ReductionHelper(statement1, statement2);
+    state1 = statement1;
+    state2 = statement2;
   } else if (getString(statement2).length > getString(statement1).length) {
-    return ReductionHelper(statement2, statement1);
+    state1 = statement2;
+    state2 = statement1;
   } else {
     return false;
   }
+  return FindChanges(state1, state2, getString(state1), getString(state2));
+}
+
+function FindChanges(state1, state2, str1, str2) {
+  if (ReductionHelper(state1, state2)) {
+    return true;
+  }
+  let spot = -1;
+  for (let i=0; i<state2.parts.length; i++) {
+    if (state2.type === state1.type) {
+      if (state2.parts[i].type !== state1.parts[i].type || getString(state2.parts[i]) !== getString(state1.parts[i])) {
+        spot = i
+      }
+    } else {
+      return ReductionHelper(state1, state2);
+    }
+  }
+  if (spot === -1) {
+    return false;
+  } else {
+    if (FindChanges(state1.parts[spot], state2.parts[spot], str1, str2)) {
+      if (str1.replace(getString(state1.parts[spot]), getString(state2.parts[spot])) === str2) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 function ReductionHelper(statement1, statement2) {
@@ -34,23 +65,26 @@ function ReductionHelper(statement1, statement2) {
   reductStr = reductStr.replace("AND", "&");
   reductStr = reductStr.replace("OR", "|");
   reductStr = reductStr.replace("NOT", "~");
-  console.log(reductStr);
   return (reductStr === getString(statement2));
 }
 
 let statement1 = getParsedStatement("A&(~A|B)")
-let statement2 = getParsedStatement("A&B")
+let statement2 = getParsedStatement("A&B")//true
 
 let statement3 = getParsedStatement("A|(~A&B)")
-let statement4 = getParsedStatement("A|B")
+let statement4 = getParsedStatement("A|B")//true
 
 let statement5 = getParsedStatement("(A&B)|(~(A&B)&(C|D))")
-let statement6 = getParsedStatement("(A&B)|(C|D)")
+let statement6 = getParsedStatement("(A&B)|(C|D)")//true
 
 let statement7 = getParsedStatement("A|(~A&B)")
-let statement8 = getParsedStatement("A|B")
+let statement8 = getParsedStatement("A|B")//true
+
+let statement9 = getParsedStatement("C&(A|(~A&B))")
+let statement10 = getParsedStatement("C&(A|B)")//true
 
 console.log(ReductionVerifier(statement1, statement2))
 console.log(ReductionVerifier(statement3, statement4))
 console.log(ReductionVerifier(statement5, statement6))
 console.log(ReductionVerifier(statement7, statement8))
+console.log(ReductionVerifier(statement9, statement10))
