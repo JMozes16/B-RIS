5ca7c5f2ab97589e4db12dc4eacce348fd03f50c
Josh Mozes <85561037+JMozes16@users.noreply.github.com>
2022-04-29 01:18:44 -0400

    FindChanges Complement

--- src/boolean_algebra/verifiers/Complement.js
+++ src/boolean_algebra/verifiers/Complement.js
@@ -1,7 +1,40 @@
 import {getParsedStatement, getString} from "../Parser.js";
 
 export function ComplementVerifier(statement1, statement2) {
-  return (ComplementHelper(statement1, statement2) || ComplementHelper(statement1, statement2));
+  return FindChanges(statement1, statement2);
+}
+
+function FindChanges(statement1, statement2) {
+  let state1 = statement1;
+  let state2 = statement2;
+  if (getString(statement1).length > getString(statement2).length) {
+    state1 = statement1;
+    state2 = statement2;
+  } else if (getString(statement2).length > getString(statement1).length) {
+    state1 = statement2;
+    state2 = statement1;
+  } else {
+    return false;
+  }
+
+  if (state2.type === "ATOMIC") {
+    return ComplementHelper(state1, state2);
+  }
+  let spot = -1;
+  for (let i=0; i<state2.parts.length; i++) {
+    if (state2.type === state1.type) {
+      if (state2.parts[i].type !== state1.parts[i].type || getString(state2.parts[i]) !== getString(state1.parts[i])) {
+        spot = i;
+      }
+    } else {
+      return ComplementHelper(state1, state2);
+    }
+  }
+  if (spot === -1) {
+    return false;
+  } else {
+    return FindChanges(state1.parts[spot], state2.parts[spot]);
+  }
 }
 
 function ComplementHelper(statement1, statement2) {
@@ -44,8 +77,16 @@ let statement8 = getParsedStatement("⊤")//false
 let statement9 = getParsedStatement("(A&B)|~((A&B)&C)")
 let statement10 = getParsedStatement("⊤")//false
 
+let statement11 = getParsedStatement("A&(B|((A&B)|~(A&B)))")
+let statement12 = getParsedStatement("A&(B|⊤)")//true
+
+let statement13 = getParsedStatement("A|((A&B)|~(A&B))")
+let statement14 = getParsedStatement("A|⊤")//true
+
 console.log(ComplementVerifier(statement1, statement2))
 console.log(ComplementVerifier(statement3, statement4))
 console.log(ComplementVerifier(statement5, statement6))
 console.log(ComplementVerifier(statement7, statement8))
 console.log(ComplementVerifier(statement9, statement10))
+console.log(ComplementVerifier(statement11, statement12))
+console.log(ComplementVerifier(statement13, statement14))
