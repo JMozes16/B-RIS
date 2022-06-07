b7419ce2ee6759d105bff5931038bc3c3bbe7b68
Josh Mozes <85561037+JMozes16@users.noreply.github.com>
2022-06-07 19:53:26 -0400

    Cleanup

--- src/boolean_algebra/verifiers/Adjacency/Adjacency.js
+++ src/boolean_algebra/verifiers/Adjacency/Adjacency.js
@@ -1,4 +1,4 @@
-import {getString} from "../../Parser.js";
+import {getString, findChanges} from "../../Parser.js";
 
 export function AdjacencyVerifier(statement1, statement2) {
   let state1 = statement1;
@@ -12,45 +12,10 @@ export function AdjacencyVerifier(statement1, statement2) {
   } else {
     return false;
   }
-  return FindChanges(state1, state2, getString(state1), getString(state2));
+  return findChanges(state1, state2, AdjacencyHelper);
 }
 
-function FindChanges(state1, state2, str1, str2) {
-  if (AdjacencyHelper(state1, state2)) {
-    return true;
-  }
-  let spot = -1;
-  if (!state2.type) {
-    return false;
-  }
-  for (let i=0; i<state2.parts.length; i++) {
-    if (state2.type === state1.type) {
-      if (state2.type === "ATOMIC") {
-        if (state2.parts[i].type !== state1.parts[i].type || state2.parts[i] !== state1.parts[i]) {
-          spot = i;
-        }
-      } else {
-        if (state2.parts[i].type !== state1.parts[i].type || getString(state2.parts[i]) !== getString(state1.parts[i])) {
-          spot = i;
-        }
-      }
-    } else {
-      return AdjacencyHelper(state1, state2);
-    }
-  }
-  if (spot === -1) {
-    return false;
-  } else {
-    if (FindChanges(state1.parts[spot], state2.parts[spot], str1, str2)) {
-      if (str1.replace(getString(state1.parts[spot]), getString(state2.parts[spot])) === str2) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-function AdjacencyHelper(statement1, statement2) {
+export function AdjacencyHelper(statement1, statement2) {
   if (!statement1.type || statement1.type === "ATOMIC") {
     return false;
   }
--- src/boolean_algebra/verifiers/Annihilation/Annihilation.js
+++ src/boolean_algebra/verifiers/Annihilation/Annihilation.js
@@ -1,4 +1,4 @@
-import {getString} from "../../Parser.js";
+import {getString, findChanges} from "../../Parser.js";
 
 export function AnnihilationVerifier(statement1, statement2) {
   let state1 = statement1;
@@ -12,45 +12,10 @@ export function AnnihilationVerifier(statement1, statement2) {
   } else {
     return false;
   }
-  return FindChanges(state1, state2, getString(state1), getString(state2));
+  return findChanges(state1, state2, AnnihilationHelper);
 }
 
-function FindChanges(state1, state2, str1, str2) {
-  if (AnnihilationHelper(state1, state2)) {
-    return true;
-  }
-  let spot = -1;
-  if (!state2.type) {
-    return false;
-  }
-  for (let i=0; i<state2.parts.length; i++) {
-    if (state2.type === state1.type) {
-      if (state2.type === "ATOMIC") {
-        if (state2.parts[i].type !== state1.parts[i].type || state2.parts[i] !== state1.parts[i]) {
-          spot = i;
-        }
-      } else {
-        if (state2.parts[i].type !== state1.parts[i].type || getString(state2.parts[i]) !== getString(state1.parts[i])) {
-          spot = i;
-        }
-      }
-    } else {
-      return AnnihilationHelper(state1, state2);
-    }
-  }
-  if (spot === -1) {
-    return false;
-  } else {
-    if (FindChanges(state1.parts[spot], state2.parts[spot], str1, str2)) {
-      if (str1.replace(getString(state1.parts[spot]), getString(state2.parts[spot])) === str2) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-function AnnihilationHelper(statement1,statement2){
+export function AnnihilationHelper(statement1,statement2){
   if (!statement1.type || statement1.type === "ATOMIC") {
     return false;
   }
