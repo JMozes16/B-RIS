c725233113af1b1f0cca8453883640da961d05cc
mickannor <89411564+mickannor@users.noreply.github.com>
2022-06-24 13:59:34 -0400

    Update Commutation.test.js

--- src/boolean_algebra/verifiers/Commutation/Commutation.test.js
+++ src/boolean_algebra/verifiers/Commutation/Commutation.test.js
@@ -6,3 +6,87 @@ test('Test 1', () => {
   let statement2 = getParsedStatement("B|A")
   expect(CommutationVerifier(statement1, statement2)).toBeTruthy();
 });
+
+test('Test 2', () => {
+  let statement1 = getParsedStatement("B|A")
+  let statement2 = getParsedStatement("A|B")
+  expect(CommutationVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 3', () => {
+  let statement1 = getParsedStatement("A & B")
+  let statement2 = getParsedStatement("B & A")
+  expect(CommutationVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 4', () => {
+  let statement1 = getParsedStatement("B & A")
+  let statement2 = getParsedStatement("A & B")
+  expect(CommutationVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 5', () => {
+  let statement1 = getParsedStatement("(A & B) | C")
+  let statement2 = getParsedStatement("(B & A) | C")
+  expect(CommutationVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 6', () => {
+  let statement1 = getParsedStatement("(A | C) & B")
+  let statement2 = getParsedStatement("(C | A) & B")
+  expect(CommutationVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 7', () => {
+  let statement1 = getParsedStatement("(A & B) | (B & C)")
+  let statement2 = getParsedStatement("(B & A) | (B & C)")
+  expect(CommutationVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 8', () => {
+  let statement1 = getParsedStatement("a & b & c")
+  let statement2 = getParsedStatement("c & a & b")
+  expect(CommutationVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 9', () => {
+  let statement1 = getParsedStatement("A & B & C")
+  let statement2 = getParsedStatement("a & b & c")
+  expect(CommutationVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 10', () => {
+  let statement1 = getParsedStatement("(A & B) | C")
+  let statement2 = getParsedStatement("(A & C) | B")
+  expect(CommutationVerifier(statement1, statement2)).toBeFalsy(); 
+});
+
+test('Test 11', () => {
+  let statement1 = getParsedStatement("(A & B) | C")
+  let statement2 = getParsedStatement("(A|C) & B")
+  expect(CommutationVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 12', () => {
+  let statement1 = getParsedStatement("A|B|C")
+  let statement2 = getParsedStatement("A & B & C")
+  expect(CommutationVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 13', () => {
+  let statement1 = getParsedStatement("a|B")
+  let statement2 = getParsedStatement("b|A")
+  expect(CommutationVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 14', () => {
+  let statement1 = getParsedStatement()
+  let statement2 = getParsedStatement()
+  expect(CommutationVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 15', () => {
+  let statement1 = getParsedStatement()
+  let statement2 = getParsedStatement()
+  expect(CommutationVerifier(statement1, statement2)).toBeFalsy();
+});
