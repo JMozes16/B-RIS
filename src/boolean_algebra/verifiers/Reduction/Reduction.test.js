352e42d8f360b23463fe47a659b7525b19afca05
mickannor <89411564+mickannor@users.noreply.github.com>
2022-07-12 14:16:41 -0400

    Update Reduction.test.js

--- src/boolean_algebra/verifiers/Reduction/Reduction.test.js
+++ src/boolean_algebra/verifiers/Reduction/Reduction.test.js
@@ -36,3 +36,39 @@ test('Test 6', () => {
   let statement2 = getParsedStatement("C&(A|B)&D")
   expect(ReductionVerifier(statement1, statement2)).toBeTruthy();
 });
+
+test('Test 7', () => {
+  let statement1 = getParsedStatement("A&(~A|B)")
+  let statement2 = getParsedStatement("(A&~A)|(A&B)")
+  expect(ReductionVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 8', () => {
+  let statement1 = getParsedStatement("~A&(C|D)")
+  let statement2 = getParsedStatement("C&D")
+  expect(ReductionVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 9', () => {
+  let statement1 = getParsedStatement("A&(A|B|~A)")
+  let statement2 = getParsedStatement("A|B")
+  expect(ReductionVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 10', () => {
+  let statement1 = getParsedStatement("(~A&A)|(b&~b)|(c&~C)")
+  let statement2 = getParsedStatement("c")
+  expect(ReductionVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 11', () => {
+  let statement1 = getParsedStatement("(c&~C)")
+  let statement2 = getParsedStatement("")
+  expect(ReductionVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 12', () => {
+  let statement1 = getParsedStatement("(A|~A)&(~A|A)")
+  let statement2 = getParsedStatement("A&A")
+  expect(ReductionVerifier(statement1, statement2)).toBeFalsy();
+});
