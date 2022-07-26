234680dfc5c41f43c896dac172fdb537454b2b39
Jeda Coward <95391501+jeda234@users.noreply.github.com>
2022-07-26 14:25:20 -0400

    Update DeMorgan Test Suite (#25)
    Added more test cases and updated findChanges in the DeMorgan verifier

--- src/boolean_algebra/verifiers/DeMorgan/DeMorgan.test.js
+++ src/boolean_algebra/verifiers/DeMorgan/DeMorgan.test.js
@@ -6,3 +6,93 @@ test('Test 1', () => {
   let statement2 = getParsedStatement("~A&~B")
   expect(DeMorganVerifier(statement1, statement2)).toBeTruthy();
 });
+
+test('Test 2', () => {
+  let statement1 = getParsedStatement("~(A&B)")
+  let statement2 = getParsedStatement("~A|~B")
+  expect(DeMorganVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 3', () => {
+  let statement1 = getParsedStatement("~((A&B)|(C&D))")
+  let statement2 = getParsedStatement("~(A&B)&~(C&D)")
+  expect(DeMorganVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 4', () => {
+  let statement1 = getParsedStatement("~((A&B)|(C&D))")
+  let statement2 = getParsedStatement("(~A&~B)&(~C&~D)")
+  expect(DeMorganVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 5', () => {
+  let statement1 = getParsedStatement("~((A&B)|(C&D)|(E&G))")
+  let statement2 = getParsedStatement("~(A&B)&~(C&D)&~(E&G)")
+  expect(DeMorganVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 6', () => {
+  let statement1 = getParsedStatement("~(A|(B&C))")
+  let statement2 = getParsedStatement("~A&~(B&C)")
+  expect(DeMorganVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 7', () => {
+  let statement1 = getParsedStatement("~(A|(B&C&~D))")
+  let statement2 = getParsedStatement("~A&~(B&C&~D)")
+  expect(DeMorganVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 8', () => {
+  let statement1 = getParsedStatement("~(A|(B&C&~D))")
+  let statement2 = getParsedStatement("~A&~(F&C&~D)")
+  expect(DeMorganVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 9', () => {
+  let statement1 = getParsedStatement("~(A|(B&C)|D)")
+  let statement2 = getParsedStatement("~A&~(B&C)&~D")
+  expect(DeMorganVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 10', () => {
+  let statement2 = getParsedStatement("~(A|(B&C)|(D&E))")
+  let statement1 = getParsedStatement("~A&~(B&C)&~(D&E)")
+  expect(DeMorganVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 11', () => {
+  let statement1 = getParsedStatement("(~(A|B)|~(C|D))&G")
+  let statement2 = getParsedStatement("~((A|B)&(C|D))&G")
+  expect(DeMorganVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 12', () => {
+  let statement1 = getParsedStatement("(~((A|B))|~((C|D)))&G")
+  let statement2 = getParsedStatement("((~A&~B)|(~C&~D))&G")
+  expect(DeMorganVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 13', () => {
+  let statement1 = getParsedStatement("~((A&G)|(B&C)|(D&E))")
+  let statement2 = getParsedStatement("~(P&G)&~(B&C)&~(D&E)")
+  expect(DeMorganVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 14', () => {
+  let statement1 = getParsedStatement("~(A|((B&C)&(D&~E)))")
+  let statement2 = getParsedStatement("~A&(~(B&C)&(D&~E))")
+  expect(DeMorganVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 15', () => {
+  let statement1 = getParsedStatement("~((A&G)|(B&C)|(D&E))")
+  let statement2 = getParsedStatement("~(A&G)|~(B&C)|~(D&E)")
+  expect(DeMorganVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 16', () => {
+  let statement1 = getParsedStatement("(~(A|B)|(C|D))&G")
+  let statement2 = getParsedStatement("~((A|B)&(C|D))&G")
+  expect(DeMorganVerifier(statement1, statement2)).toBeFalsy();
+});
