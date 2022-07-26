c51e133faf836d937f972ad122c1e78fe50ffde8
Josh Mozes <85561037+JMozes16@users.noreply.github.com>
2022-07-26 14:20:32 -0400

    Inverse test suite (#30)
    * test suite
    
    * fix no change inverse
    
    * fix for deployment

--- src/boolean_algebra/verifiers/Inverse/Inverse.test.js
+++ src/boolean_algebra/verifiers/Inverse/Inverse.test.js
@@ -12,3 +12,99 @@ test('Test 2', () => {
   let statement2 = getParsedStatement("⊤")
   expect(InverseVerifier(statement1, statement2)).toBeTruthy();
 });
+
+test('Test 3', () => {
+  let statement1 = getParsedStatement("A&~⊥")
+  let statement2 = getParsedStatement("A&⊤")
+  expect(InverseVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 4', () => {
+  let statement1 = getParsedStatement("A&~⊥")
+  let statement2 = getParsedStatement("B&⊤")
+  expect(InverseVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 5', () => {
+  let statement1 = getParsedStatement("A&B&~⊥")
+  let statement2 = getParsedStatement("A&B&⊤")
+  expect(InverseVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 6', () => {
+  let statement1 = getParsedStatement("A&B&(C|~⊥)")
+  let statement2 = getParsedStatement("A&B&(C|⊤)")
+  expect(InverseVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 7', () => {
+  let statement1 = getParsedStatement("A&B&(C|~⊥)")
+  let statement2 = getParsedStatement("A&B&(C&⊤)")
+  expect(InverseVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 8', () => {
+  let statement1 = getParsedStatement("A&B&(C|~⊥)")
+  let statement2 = getParsedStatement("A&B&(D|⊤)")
+  expect(InverseVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 9', () => {
+  let statement1 = getParsedStatement("(A&B)&~⊥")
+  let statement2 = getParsedStatement("(A&B&C)&⊤")
+  expect(InverseVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 10', () => {
+  let statement1 = getParsedStatement("~⊥&B&~⊥")
+  let statement2 = getParsedStatement("~⊥&B&⊤")
+  expect(InverseVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 11', () => {
+  let statement1 = getParsedStatement("~⊥&B&~⊥")
+  let statement2 = getParsedStatement("⊤&B&⊤")
+  expect(InverseVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 12', () => {
+  let statement1 = getParsedStatement("~⊥&B&~⊥")
+  let statement2 = getParsedStatement("~⊥&B&~⊥")
+  expect(InverseVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 13', () => {
+  let statement1 = getParsedStatement("~⊥&B&~⊥")
+  let statement2 = getParsedStatement("⊤&B&~⊤")
+  expect(InverseVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 14', () => {
+  let statement1 = getParsedStatement("(~⊥&B)&~⊥")
+  let statement2 = getParsedStatement("(⊤&B)&⊤")
+  expect(InverseVerifier(statement1, statement2)).toBeTruthy();
+});
+
+test('Test 15', () => {
+  let statement1 = getParsedStatement("(~⊥&B)&~⊥")
+  let statement2 = getParsedStatement("(B&⊤)&~⊤")
+  expect(InverseVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 16', () => {
+  let statement1 = getParsedStatement("~⊥")
+  let statement2 = getParsedStatement("~⊤")
+  expect(InverseVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 17', () => {
+  let statement1 = getParsedStatement("~⊥")
+  let statement2 = getParsedStatement("~⊥")
+  expect(InverseVerifier(statement1, statement2)).toBeFalsy();
+});
+
+test('Test 18', () => {
+  let statement1 = getParsedStatement("⊤")
+  let statement2 = getParsedStatement("⊤")
+  expect(InverseVerifier(statement1, statement2)).toBeFalsy();
+});
